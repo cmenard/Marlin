6110ec1cf0cecaae12deecd4b529c81fb85b2e39
Curtis Menard <curtis.menard@gmail.com>
2020-05-04 15:47:08 -0400

    bump the Junction_Deviation_MM and enable S-Curve Acceleration.  Needed to get the speed back in the printer.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -801,7 +801,7 @@
  *   http://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM 0.0128 // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.020 // (mm) .4*10*10/2000=0.020 Distance from real junction edge default 0.013
 #endif
 
 /**
@@ -812,7 +812,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-//#define S_CURVE_ACCELERATION
+#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
