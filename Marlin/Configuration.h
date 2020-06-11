e41e01286530a5580d022214a7312be8859ebd66
Curtis Menard <curtis.menard@gmail.com>
2020-06-10 21:22:15 -0400

    True bed size numbers for soft endstops.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1093,8 +1093,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 525
-#define Y_BED_SIZE 525
+#define X_BED_SIZE 450
+#define Y_BED_SIZE 450
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
