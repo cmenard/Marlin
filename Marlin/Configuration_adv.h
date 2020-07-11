867a3d12f283b0c756dba559f5e5bfc896784327
Curtis Menard <curtis.menard@gmail.com>
2020-07-11 17:34:58 -0400

    Fix using onboard SDCard from TFT screen.  Allows gcode to be run from onboard SDCard or SDCard located in TFT screen.  Also added long filename support.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1119,7 +1119,7 @@
   #endif
 
   // This allows hosts to request long names for files and folders with M33
-  //#define LONG_FILENAME_HOST_SUPPORT
+  #define LONG_FILENAME_HOST_SUPPORT
 
   // Enable this option to scroll long filenames in the SD card menu
   #define SCROLL_LONG_FILENAMES
@@ -1206,7 +1206,7 @@
    *
    * :[ 'LCD', 'ONBOARD', 'CUSTOM_CABLE' ]
    */
-  //#define SDCARD_CONNECTION LCD
+  #define SDCARD_CONNECTION ONBOARD
 
 #endif // SDSUPPORT
 
